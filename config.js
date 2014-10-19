d85981118ac9a2c1f7b6bea6b0e80b9b24d7866d
Jeff Douglas <jeff@jeffdouglas.com>
2014-10-19 10:22:08 -0400

    added env to configs

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ exports.ENVIRONMENT = 'production';
 exports.CALLBACK_URL = 'http://localhost:3001';
 exports.PUSH_TOPIC = 'AllAccounts';
 
-exports.CLIENT_ID = YOUR-SFDC-CLIENT-ID;
-exports.CLIENT_SECRET = YOUR-SFDC-CLIENT-SECRET;
-exports.USERNAME = YOUR-SFDC-USERNAME;
-exports.PASSWORD = YOUR-SFDC-PASSWORD-AND-TOKEN;
+exports.CLIENT_ID = process.env.CLIENT_ID || "YOUR-SFDC-CLIENT-ID";
+exports.CLIENT_SECRET = process.env.CLIENT_SECRET || "YOUR-SFDC-CLIENT-SECRET";
+exports.USERNAME = process.env.USERNAME || "YOUR-SFDC-USERNAME";
+exports.PASSWORD = process.env.PASSWORD || "YOUR-SFDC-PASSWORD-AND-TOKEN";
